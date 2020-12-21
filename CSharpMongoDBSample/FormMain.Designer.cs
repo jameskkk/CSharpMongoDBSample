a5e31e9675279db361a69f335ee7efd4735086e6
jameskkk <chenjhkk@gmail.com>
2020-12-21 14:46:13 +0800

    Import C# MongoDB Sample into github.

---
+++ CSharpMongoDBSample/FormMain.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace CSharpMongoDBTest
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.btnInsertOne = new System.Windows.Forms.Button();
+            this.btnStatus = new System.Windows.Forms.Button();
+            this.btnListDB = new System.Windows.Forms.Button();
+            this.btnQueryAll = new System.Windows.Forms.Button();
+            this.btnSkipLimit = new System.Windows.Forms.Button();
+            this.btnProjections = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsertMany = new System.Windows.Forms.Button();
+            this.btnDropDB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(49, 113);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(141, 54);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // btnInsertOne
+            // 
+            this.btnInsertOne.Location = new System.Drawing.Point(49, 195);
+            this.btnInsertOne.Name = "btnInsertOne";
+            this.btnInsertOne.Size = new System.Drawing.Size(141, 54);
+            this.btnInsertOne.TabIndex = 1;
+            this.btnInsertOne.Text = "Insert One";
+            this.btnInsertOne.UseVisualStyleBackColor = true;
+            this.btnInsertOne.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.Location = new System.Drawing.Point(49, 30);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(141, 54);
+            this.btnStatus.TabIndex = 2;
+            this.btnStatus.Text = "Status";
+            this.btnStatus.UseVisualStyleBackColor = true;
+            this.btnStatus.Click += new System.EventHandler(this.btnStatus_Click);
+            // 
+            // btnListDB
+            // 
+            this.btnListDB.Location = new System.Drawing.Point(282, 30);
+            this.btnListDB.Name = "btnListDB";
+            this.btnListDB.Size = new System.Drawing.Size(141, 54);
+            this.btnListDB.TabIndex = 3;
+            this.btnListDB.Text = "List DB";
+            this.btnListDB.UseVisualStyleBackColor = true;
+            this.btnListDB.Click += new System.EventHandler(this.btnListDB_Click);
+            // 
+            // btnQueryAll
+            // 
+            this.btnQueryAll.Location = new System.Drawing.Point(282, 113);
+            this.btnQueryAll.Name = "btnQueryAll";
+            this.btnQueryAll.Size = new System.Drawing.Size(141, 54);
+            this.btnQueryAll.TabIndex = 4;
+            this.btnQueryAll.Text = "Query All";
+            this.btnQueryAll.UseVisualStyleBackColor = true;
+            this.btnQueryAll.Click += new System.EventHandler(this.btnQueryAll_Click);
+            // 
+            // btnSkipLimit
+            // 
+            this.btnSkipLimit.Location = new System.Drawing.Point(282, 280);
+            this.btnSkipLimit.Name = "btnSkipLimit";
+            this.btnSkipLimit.Size = new System.Drawing.Size(141, 54);
+            this.btnSkipLimit.TabIndex = 5;
+            this.btnSkipLimit.Text = "Skip and Limit";
+            this.btnSkipLimit.UseVisualStyleBackColor = true;
+            this.btnSkipLimit.Click += new System.EventHandler(this.btnSkipLimit_Click);
+            // 
+            // btnProjections
+            // 
+            this.btnProjections.Location = new System.Drawing.Point(49, 280);
+            this.btnProjections.Name = "btnProjections";
+            this.btnProjections.Size = new System.Drawing.Size(141, 54);
+            this.btnProjections.TabIndex = 6;
+            this.btnProjections.Text = "Projections";
+            this.btnProjections.UseVisualStyleBackColor = true;
+            this.btnProjections.Click += new System.EventHandler(this.btnProjections_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(49, 360);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(141, 54);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete ";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(49, 432);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(141, 54);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsertMany
+            // 
+            this.btnInsertMany.Location = new System.Drawing.Point(282, 195);
+            this.btnInsertMany.Name = "btnInsertMany";
+            this.btnInsertMany.Size = new System.Drawing.Size(141, 54);
+            this.btnInsertMany.TabIndex = 9;
+            this.btnInsertMany.Text = "Insert Many";
+            this.btnInsertMany.UseVisualStyleBackColor = true;
+            this.btnInsertMany.Click += new System.EventHandler(this.btnInsertMany_Click);
+            // 
+            // btnDropDB
+            // 
+            this.btnDropDB.Location = new System.Drawing.Point(282, 360);
+            this.btnDropDB.Name = "btnDropDB";
+            this.btnDropDB.Size = new System.Drawing.Size(141, 54);
+            this.btnDropDB.TabIndex = 10;
+            this.btnDropDB.Text = "Drop DB";
+            this.btnDropDB.UseVisualStyleBackColor = true;
+            this.btnDropDB.Click += new System.EventHandler(this.btnDropDB_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 498);
+            this.Controls.Add(this.btnDropDB);
+            this.Controls.Add(this.btnInsertMany);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnProjections);
+            this.Controls.Add(this.btnSkipLimit);
+            this.Controls.Add(this.btnQueryAll);
+            this.Controls.Add(this.btnListDB);
+            this.Controls.Add(this.btnStatus);
+            this.Controls.Add(this.btnInsertOne);
+            this.Controls.Add(this.btnQuery);
+            this.Name = "FormMain";
+            this.Text = "CSharpMongoDB Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Button btnInsertOne;
+        private System.Windows.Forms.Button btnStatus;
+        private System.Windows.Forms.Button btnListDB;
+        private System.Windows.Forms.Button btnQueryAll;
+        private System.Windows.Forms.Button btnSkipLimit;
+        private System.Windows.Forms.Button btnProjections;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsertMany;
+        private System.Windows.Forms.Button btnDropDB;
+    }
+}
+
---
+++ CSharpMongoDBSample/FormMain.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace CSharpMongoDBTest
+{
+
+    /// <summary>
+    /// Install MongoDB.Driver and MongoDB.Bson from Nuget
+    /// </summary>
+    public partial class FormMain : Form
+    {
+        //private string m_IPAddress = "10.2.21.39";
+        private string m_IPAddress = "127.0.0.1";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+            var cars = db.GetCollection<BsonDocument>("cars");
+
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Gt("price", 30000) & builder.Lt("price", 55000);
+
+            var docs = cars.Find(filter).ToList();
+
+            docs.ForEach(doc => {
+                Console.WriteLine(doc);
+            });
+
+            Console.WriteLine("Query end!");
+        }
+
+        private void btnQueryAll_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+
+            var cars = db.GetCollection<BsonDocument>("cars");
+            var documents = cars.Find(new BsonDocument()).ToList();
+
+            foreach (BsonDocument doc in documents)
+            {
+                Console.WriteLine(doc.ToString());
+            }
+
+            Console.WriteLine("Query all end!");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+            var cars = db.GetCollection<BsonDocument>("cars");
+            var doc = new BsonDocument
+            {
+                {"name", "BMW"},
+                {"price", 34621}
+            };
+
+            cars.InsertOne(doc);
+
+            Console.WriteLine("Insert One end!");
+        }
+
+        /*
+            { "name" : "Audi", "price" : 52000 }
+            { "name" : "Mercedes", "price" : 57127.0 }
+            { "name" : "Volvo", "price" : 29000.0 }
+            { "name" : "Bentley", "price" : 350000.0 }
+            { "name" : "Citroen", "price" : 21000.0 }
+            { "name" : "Hummer", "price" : 41400.0 }
+            { "name" : "Volkswagen", "price" : 21600.0 }
+        */
+        private void btnInsertMany_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+            var cars = db.GetCollection<BsonDocument>("cars");
+            var carsList = new List<BsonDocument>()
+            {
+                new BsonDocument
+                {
+                    {"name", "BMW"},
+                    {"price", 34621},
+                },
+                new BsonDocument
+                {
+                    {"name", "Audi"},
+                    {"price", 52000}
+                },
+                new BsonDocument
+                {
+                    {"name", "Mercedes"},
+                    {"price", 57127.0}
+                },
+                new BsonDocument
+                {
+                    {"name", "Volvo"},
+                    {"price", 29000.0}
+                },
+                new BsonDocument
+                {
+                    {"name", "Bentley"},
+                    {"price", 350000.0}
+                },
+                new BsonDocument
+                {
+                    {"name", "Citroen"},
+                    {"price", 21000.0}
+                },
+                new BsonDocument
+                {
+                    {"name", "Hummer"},
+                    {"price", 41400.0}
+
+                },
+                new BsonDocument
+                {
+                    {"name", "Volkswagen"},
+                    {"price", 21600.0},
+                    {"note", "Kent's car"}
+                },
+                new BsonDocument
+                {
+                    {"name", "Lexus"},
+                    {"price", 41600.0},
+                    {"note", "John's car"}
+                }
+            };
+           
+            cars.InsertMany(carsList);
+
+            Console.WriteLine("Insert Many end!");
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+            var command = new BsonDocument { { "dbstats", 1 } };
+            var result = db.RunCommand<BsonDocument>(command);
+            Console.WriteLine(result.ToJson());
+
+            Console.WriteLine("Get Status end!");
+        }
+
+        private void btnListDB_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases are:");
+
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("List DB end!");
+        }
+
+        private void btnSkipLimit_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+            var cars = db.GetCollection<BsonDocument>("cars");
+            var docs = cars.Find(new BsonDocument()).Skip(3).Limit(3).ToList();
+
+            docs.ForEach(doc =>
+            {
+                Console.WriteLine(doc);
+            });
+
+            Console.WriteLine("Skip and Limit end!");
+        }
+
+        private void btnProjections_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+
+            var cars = db.GetCollection<BsonDocument>("cars");
+            var docs = cars.Find(new BsonDocument()).Project("{_id: 0}").ToList();
+
+            docs.ForEach(doc =>
+            {
+                Console.WriteLine(doc);
+            });
+
+            Console.WriteLine("Projections end!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+
+            var cars = db.GetCollection<BsonDocument>("cars");
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "BMW");
+
+            cars.DeleteOne(filter);
+
+            Console.WriteLine("Delete end!");
+        }
+
+        private void btnDropDB_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+
+            db.DropCollection("cars");
+            //var cars = db.GetCollection<BsonDocument>("cars");
+            //cars.Drop();
+
+            Console.WriteLine("DropDB end!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // 預設連線(localhost: 27017)
+            string url = "mongodb://" + m_IPAddress + ":27017";
+            Console.WriteLine("url = " + url);
+            var dbClient = new MongoClient(url);
+
+            IMongoDatabase db = dbClient.GetDatabase("testdb");
+
+            var cars = db.GetCollection<BsonDocument>("cars");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "Audi");
+            var update = Builders<BsonDocument>.Update.Set("price", 52000);
+
+            cars.UpdateOne(filter, update);
+
+            Console.WriteLine("Update end!");
+        }
+    }
+}
